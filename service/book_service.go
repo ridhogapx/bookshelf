69ecb1c34de6fd3f8c91560edf738dac3f801be1
RageNeko26 <rneko2006@gmail.com>
2023-07-09 16:56:10 +0700

    fix: Update service method for Book service

--- service/book_service.go
+++ service/book_service.go
@@ -1,9 +1,35 @@
 package service
 
 import (
+	"bookshelf/db"
+	"bookshelf/model"
 	pb "bookshelf/proto"
+	"context"
 )
 
 type BookSrv struct {
 	pb.UnimplementedBookServiceServer
+}
+
+func (*BookSrv) CreateBook(ctx context.Context, req *pb.CreateBookRequest) (*pb.CreateBookResponse, error) {
+	book := req.GetBook()
+
+	data := model.Book{
+		ID:     book.GetId(),
+		Title:  book.GetTitle(),
+		Author: book.GetAuthor(),
+		Owner:  book.GetOwner(),
+	}
+
+	db.DB.Create(&data)
+
+	return &pb.CreateBookResponse{
+		Book: &pb.Book{
+			Id:     book.GetId(),
+			Title:  book.GetTitle(),
+			Author: book.GetAuthor(),
+			Owner:  book.GetOwner(),
+		},
+	}, nil
+
 }
