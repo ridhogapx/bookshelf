d8b6111c177f24a8a526306e8b28d34eddfcb1b5
RageNeko26 <rneko2006@gmail.com>
2023-07-10 15:17:32 +0700

    fix: Initialize route handler

--- controller/book_controller.go
+++ controller/book_controller.go
@@ -1,1 +1,30 @@
 package controller
+
+import (
+	"bookshelf/entity"
+
+	pb "bookshelf/proto"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddBook(ctx gin.Context) {
+	var book entity.BookEntity
+
+	err := ctx.ShouldBind(&book)
+
+	if err != nil {
+		ctx.JSON(400, gin.H{
+			"error": err,
+		})
+		return
+	}
+
+	data := &pb.Book{
+		Id:     book.ID,
+		Title:  book.Title,
+		Author: book.Author,
+		Owner:  book.Owner,
+	}
+
+}
