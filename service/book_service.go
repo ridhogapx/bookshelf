e656bfb560be6931f51543ba31b4bfdab458b525
RageNeko26 <rneko2006@gmail.com>
2023-07-10 18:59:09 +0700

    fix: add todo comment

--- service/book_service.go
+++ service/book_service.go
@@ -7,6 +7,14 @@ import (
 	"context"
 )
 
+/*
+* Todo:
+* Implement method for retrieve all book data.
+* Implement method for retrieve single book by its ID.
+* Implement method for update book
+* Implement method for delete book
+ */
+
 type BookSrv struct {
 	pb.UnimplementedBookServiceServer
 }
