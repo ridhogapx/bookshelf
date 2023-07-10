bd645ae76c1fc97f9ef24469434d8ab16d8c47e2
RageNeko26 <rneko2006@gmail.com>
2023-07-10 14:32:41 +0700

    fix: Initialize Client

--- client/main.go
+++ client/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"flag"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("gRPC address", "localhost:50051", "Address for dialing gRPC Server")
+)
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		panic("Failed to dialing gRPC server")
+	}
+
+	defer conn.Close()
+}
