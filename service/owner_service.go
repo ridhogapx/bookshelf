5cb5a2f277c2fde362cb8f09e847d60abe8ae2f3
RageNeko26 <rneko2006@gmail.com>
2023-07-12 01:11:59 +0700

    feat: Implement owner service method to add new record

--- service/owner_service.go
+++ service/owner_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"bookshelf/config"
+	"bookshelf/model"
 	pb "bookshelf/proto"
 	"context"
+	"errors"
 )
 
 type OwnerSrv struct {
@@ -11,5 +14,24 @@ type OwnerSrv struct {
 
 // Should add new record
 func (*OwnerSrv) CreateOwner(ctx context.Context, req *pb.CreateOwnerRequest) (*pb.CreateOwnerResponse, error) {
+	arg := req.GetOwner()
+
+	data := model.Owner{
+		ID:   arg.GetId(),
+		Name: arg.GetName(),
+	}
+
+	result := config.DB.Create(&data)
+
+	if result.Error != nil {
+		return nil, errors.New("Failed to add new record")
+	}
+
+	return &pb.CreateOwnerResponse{
+		Owner: &pb.Owner{
+			Id:   data.ID,
+			Name: data.Name,
+		},
+	}, nil
 
 }
