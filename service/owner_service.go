dd2c28b9be7d452ff791fc6b6c5b896a17877e22
RageNeko26 <rneko2006@gmail.com>
2023-07-12 00:53:03 +0700

    feat: Implement method for Owner Service

--- service/owner_service.go
+++ service/owner_service.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	pb "bookshelf/proto"
+	"context"
+)
+
+type OwnerSrv struct {
+	pb.UnimplementedOwnerServiceServer
+}
+
+// Should add new record
+func (*OwnerSrv) CreateOwner(ctx context.Context, req *pb.CreateOwnerRequest) (*pb.CreateOwnerResponse, error) {
+
+}
